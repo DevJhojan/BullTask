bd3c9ccba5fa360f1ade848774ed40b3d1c7b2bd
DevJhojan <jhojan1806@gmail.com>
2024-03-08 19:08:18 -0500

    Style of sider ok

--- src/app/components/siderbar/siderbar.component.ts
+++ src/app/components/siderbar/siderbar.component.ts
@@ -1,33 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
 
+interface Menu {
+  title: string;
+  link: string;
+}
+interface Habit {
+  title: string;
+  icon: string;
+  counter: number;
+}
+
 @Component({
   selector: 'siderbar',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
-  template: `
-    <nav>
-      <ul>
-        <li>
-          <h4>Menu</h4>
-        </li>
-      </ul>
-    </nav>
-    <nav>
-      <ul>
-        <li>
-          <h5>Habitos</h5>
-        </li>
-      </ul>
-    </nav>
-  `,
+  imports: [CommonModule],
+  templateUrl: 'siderbar.component.html',
   styleUrls: ['./siderbar.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SiderbarComponent implements OnInit {
+  menus: Menu[] = [
+    {
+      title: 'Tasks',
+      link: '/Tasks',
+    },
+    {
+      title: 'Constant task',
+      link: '/Constant_Task',
+    },
+  ];
 
-  ngOnInit(): void { }
+  habits: Habit[] = [
+    {
+      title: 'Make the bet',
+      icon: '/assets/img/Habits_home.png',
+      counter: 0,
+    },
+    {
+      title: 'Do Exercises',
+      icon: '/assets/img/Habits_excercise.png',
+      counter: 0,
+    },
+  ];
 
+  ngOnInit(): void {}
+
+  aumentCounterHabit(habit: Habit): void {
+    habit.counter++;
+  }
+
+  addHabit() {}
 }
