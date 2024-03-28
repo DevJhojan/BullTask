5ec3e7b0f613bead07bf72fed61cd6998e34ab3f
DevJhojan <jhojan1806@gmail.com>
2024-03-27 22:04:43 -0500

    intento de setTime out

--- src/app/components/TimeBomb/TimeBomb.component.ts
+++ src/app/components/TimeBomb/TimeBomb.component.ts
@@ -1,14 +1,48 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { ChangeDetectionStrategy, Component, NgModule, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {interval, Subscription} from 'rxjs';
 @Component({
   selector: 'time-bomb',
   standalone: true,
   imports: [
     CommonModule,
+    FormsModule
   ],
-  template: `<p>TimeBomb works!</p>`,
+  template: `
+    <article>
+      <h3>Bomb Time</h3>
+      <label for="counter">La bomba esplotara en {{timeRest}} minutos</label>
+      <input type="text" name="counter" id="counter" [(ngModel)]="timeRest" >
+      <button (click)="start()">
+        Start
+      </button>
+      <h2> {{timeRest}} </h2>
+    </article>
+  `,
   styleUrls: ['./TimeBomb.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TimeBombComponent { }
+export class TimeBombComponent implements OnInit {
+  timeRest: number = 0;
+  interval: any;
+  private subscription: Subscription = new Subscription();;
+  ngOnInit(): void {
+
+  }
+  start(){
+    this.subscription = interval(1000).subscribe(() => {
+      if(this.timeRest > 0){
+        this.timeRest--;
+      }
+      else{
+        this.stopTimer();
+      }
+    })
+  }
+  stopTimer() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
